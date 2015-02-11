db2695c6e64f21b0c34c5bcd761793fa8749ec4b
Jerome Danthinne <jerome@grincheux.be>
2015-02-11 17:24:06 +0100

    Update template file.

--- templates/custom_search-results.tpl.php
+++ templates/custom_search-results.tpl.php
@@ -20,23 +20,48 @@
  * @see template_preprocess_custom_search_results()
  */
 ?>
-<?php if ($search_results) : ?>
+<?php if (!empty($search_results)): ?>
+
   <h2><?php print t('Search results');?></h2>
+  
   <?php if (isset($filter) && $filter != '' && $filter_position == 'above') : ?>
     <div class="custom-search-filter">
       <?php print $filter; ?>
     </div>
   <?php endif; ?>
+  
   <ol class="search-results <?php print $module; ?>-results">
-    <?php print $search_results; ?>
+    <?php foreach ($search_results as $result): ?>
+      <li <?php print $result['attributes']; ?>>
+        <?php print render($result['title_prefix']); ?>
+        <h3 class="title">
+          <a href="<?php print $result['url']; ?>"><?php print $result['title']; ?></a>
+        </h3>
+        <?php print render($result['title_suffix']); ?>
+        <div class="search-snippet-info">
+          <?php if ($result['snippet']): ?>
+            <div class="search-snippet"><?php print $result['snippet']; ?></div>
+          <?php endif; ?>
+          <?php if ($result['info']): ?>
+            <p class="search-info"><?php print $result['info']; ?></p>
+          <?php endif; ?>
+        </div>
+      </li>
+    <?php endforeach; ?>
   </ol>
+  
   <?php if (isset($filter) && $filter != '' && $filter_position == 'below') : ?>
     <div class="custom-search-filter">
       <?php print $filter; ?>
     </div>
   <?php endif; ?>
+  
   <?php print $pager; ?>
 <?php else : ?>
   <h2><?php print t('Your search yielded no results');?></h2>
-  <?php print search_help('search#noresults', drupal_help_arg()); ?>
+  <ul>
+    <li><?php print t('Check if your spelling is correct.'); ?></li>
+    <li><?php print t('Remove quotes around phrases to search for each word individually. <em>bike shed</em> will often show more results than <em>&quot;bike shed&quot;</em>.'); ?></li>
+    <li><?php print t('Consider loosening your query with <em>OR</em>. <em>bike OR shed</em> will often show more results than <em>bike shed</em>.'); ?></li>
+  </ul>
 <?php endif; ?>
